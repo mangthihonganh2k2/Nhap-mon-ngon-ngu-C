d154982ba99a68b0c20afe7c79936f7164edb739
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2020-12-03 16:24:03 +0700

    Create ketquahoctap.c

---
+++ ketquahoctap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int V, T;
+  printf("nhap diem mon van = ");
+  scanf("%d", &V);
+  printf("nhap diem mon toan = ");
+  scanf("%d", &T);
+  printf("\nDTB = 2.d");
+  return 0;
+}
