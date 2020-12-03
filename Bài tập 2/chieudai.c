92e5ffd0fad70d5ab702daae333d2b6cd30b52b9
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2020-12-03 15:18:50 +0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int l,i,f;
+// Nhap chieu dài l theo don vi gia tri ban phim
+    printf("nhap chieu dai met (M): %d", l);
+    scanf("%d",&l );  
+    i = l * 100;
+    f = i * 100;
+    printf("\nxentimet (cm) la : %d", i );
+    printf("\nmilimet (mm) la: %d" , f);
+    return 0 ;
