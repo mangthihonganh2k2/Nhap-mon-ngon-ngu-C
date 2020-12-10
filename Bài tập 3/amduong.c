04877b7fda63a6b65b951084c603179ae8b4d94b
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2020-12-10 15:09:58 +0700

    Create amduong.c

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int a ;
+	printf("nhap so a = ",a);
+	scanf("%d", &a);
+  if(a > 0){
+  	     printf("%d duong", &a);
+  	}else if(a == 0) {
+  		printf("a = 0", a);
+  		}else { 
+  		     printf("%d am", a);
+  		}
+ 
+  getchar();
+  return 0;
+  }
