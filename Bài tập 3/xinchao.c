b69f7a6c46bf989f64e9379021c982d5e30375d0
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2020-12-24 13:52:20 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,9 @@
+#include <stdio.h> main()
+  int main() {
+  	
+	     printf("xinchao\n");
+	     for (count =1; count <=6; count++ );
+	     printf("\n \t \t xinchao");
+		  printf("\n\t\t xinchao. \n"); 
+} 
+ 
