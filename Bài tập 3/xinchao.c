40da9f5a974534591294e1fc83a52c7e17e5d8f3
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2020-12-24 14:06:12 +0700

    Update xinchao.c

--- Bài tập 3/xinchao.c
+++ Bài tập 3/xinchao.c
@@ -1,9 +1,9 @@
 #include <stdio.h> main()
   int main() {
   	int count;  
-	     printf("xinchao\n");
-	     for (count =1; count <=6; count++ );
+
+	     for (count =1; count <=10; count++ ){
 	     printf("\n \t \t xinchao");
-		  printf("\n\t\t xinchao. \n"); 
+	     }
+
 } 
- 
