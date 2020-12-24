f3ae4b5a80ea6253b13e55f4a52b2dd061600d72
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2020-12-24 16:01:26 +0700

    Create tong10.c

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main(){
+    int tong = 0;
+    for(int dem = 1; dem <=10; dem++){	
+	     tong = tong + dem;
+    	}
+    	printf("tong la: %d", tong);
+    
+    return 0;
+}
