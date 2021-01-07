688d7e8e7163f3a72e07b2f38699ffc3daa22b17
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2021-01-07 16:53:57 +0700

    Create kt01.c

---
+++ Bài tập 3/kt01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	printf("bang cuu chuong\n");
+	printf("Bang 2\n");
+	printf("2*1=2\n2*2=4\n2*3=6\n");
+	printf("Bang 3\n");
+	printf("3*1=3\n3*2=6\n3*3=9\n");
+	printf("Bang 4\n");
+	printf("4*1=4\n4*2=8\n4*3=12");
+    return 0;
+
+}
