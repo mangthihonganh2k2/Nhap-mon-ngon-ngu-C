e31b9b92baf9ae8d3b8c1e74af30bddab8a05e1f
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2021-01-22 10:22:13 +0700

    Update ham_02.c

--- Bài tập 5/ham_02.c
+++ Bài tập 5/ham_02.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
+int kiem_tra_am_duong(int a);
  //khoi ham main
 int main(){
   int n;
   printf("Nhap n = ");
   scanf("%d", &n);
-      if((n) > 0)
+  if(kiem_tra_am_duong(n) > 0)
       printf("n la so nguyen duong ");
-
-  else if ((n) < 0)
-    printf("n la so nguyen âm");
-  
-  else 
+  else if (kiem_tra_am_duong(n) < 0)
+    printf("n la so nguyen am");
+  else
     printf("n = 0");
   return 0;
+}
+int kiem_tra_am_duong(int a){
+int kiem_tra_am_duong;
+return a;
 }
