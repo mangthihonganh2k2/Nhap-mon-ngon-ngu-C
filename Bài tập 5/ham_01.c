79d60e560d1a1ac1c97f2eec45ab6781c570264b
mangthihonganh2k2 <72543326+mangthihonganh2k2@users.noreply.github.com>
2021-01-22 09:33:35 +0700

    Update ham_01.c

--- Bài tập 5/ham_01.c
+++ Bài tập 5/ham_01.c
@@ -11,3 +11,18 @@ int main(){
   int sum(int a, int b, int c){
   return a + b + c ;
 }
+#include<stdio.h>
+
+
+void sum(int a, int b, int c);
+int main(){
+  int a = 4, b = 2, c = 6;
+ int sum = a + b + c;
+ printf(" tong la: %d", sum);
+}
+
+  void sum(int a, int b, int c){
+ 
+}
+ 
+ 
